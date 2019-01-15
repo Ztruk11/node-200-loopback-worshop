5e021b0ff7e91f8ecaf2e716e620edcb126511dc
Ztruk11 <danielmkurtz@gmail.com>
2019-01-15 15:25:02 -0800

    Initial project files

---
+++ server/datasources.local.js
@@ -0,0 +1,11 @@
+module.exports = {
+  db: {
+    name: 'db',
+    connector: 'memory'
+  },
+  MongoDB: {
+    url: 'mongodb://127.0.0.1:27017/loc8r',
+    name: 'MongoDB',
+    connector: 'mongodb'
+  }
+};
